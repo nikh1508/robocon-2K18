9d16093f2e6b8dd284edc4187f520a157f780860
Aditya Jyoti Paul <adityajyotipaul007@gmail.com>
2017-12-09 16:53:15 +0530

    Extract values from Red line folower

---
+++ RedLineFollower.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#define uchar unsigned char
+uchar t;
+//void send_data(short a1,short b1,short c1,short d1,short e1,short f1);
+uchar data[16];
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  t = 0;
+}
+void loop()
+{
+  Wire.requestFrom(9, 16);    // request 16 bytes from slave device #9
+  while (Wire.available())   // slave may send less than requested
+  {
+    data[t] = Wire.read(); // receive a byte as character
+    if (t < 15)
+      t++;
+    else
+      t = 0;
+  }
+  for (int i = 0; i < 15; i+=2)
+  {
+    Serial.print(data[i]);
+    Serial.print ('\t');
+  }
+  Serial.println();
+  delay(500);
+}
