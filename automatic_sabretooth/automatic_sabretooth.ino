78767b6c4ad7b3b502ebe96b9310ce8a8c9a3396
Rishav <rishav.kundu98@gmail.com>
2018-02-25 15:22:38 +0530

    [automatic_sabretooth] encoder

--- automatic_sabretooth/automatic_sabretooth.ino
+++ automatic_sabretooth/automatic_sabretooth.ino
@@ -10,15 +10,26 @@
 #define STOP while (1);
 #define CW 1
 #define CCW 0
+#define ENCODER_SAMPLE_TIME 30
 
-constexpr double kp = 50;
-constexpr double kd = 2;
-constexpr double ki = 2;
+void backward_enc(int,int,int,bool fence=false);
+void forward_enc(int, int, int, int offset=1500);
+
+constexpr double kp = 100;
+constexpr double kd = 10;
+constexpr double ki = 10;
 constexpr int BRAKE = INT_MIN;
+
+constexpr double ALPHA = 0.9;
 
 constexpr int MONSTER_PWM = 10;
 constexpr int MONSTER_L1 = 9;
 constexpr int MONSTER_L2 = 11;
+
+constexpr int TCRT1 = 6;
+constexpr int TCRT2 = 5;
+
+constexpr int LINE_THRES = 90;
 
 double setpoint = 0.0;
 double input = 0.0;
@@ -46,13 +57,16 @@ struct motors {
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
 PID myPID = PID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
 
-void setup() {
+void forward(int speed, int linecount, double target, int ign = 0);
+void fwd_sun(int speed, int linecount, double target, int ignore = 0);
+void back_sun(int speed, int linecount, double target, int ignore = 0);
 
+void setup() {
   delay(2000);
-  
+
   Serial.begin(9600);
   Serial1.begin(9600);
-  Serial2.begin(9600);
+  Serial3.begin(9600);
 
   ramp1(0);
   ramp2(0);
@@ -64,63 +78,71 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(3, INPUT);
 
+  Serial.println("before bno...");
   if (!bno.begin()) {
     Serial.println("BNO not found.");
     while (1);
   }
+  Serial.println("after bno...");
 
   bno.setExtCrystalUse(true);
-
   attachInterrupt(digitalPinToInterrupt(3), isr2, RISING);
-
+  reset_encoder();
   Serial.println("Starting bot...");
-
 }
 
 void loop() {
-  // A ->  B
-  forward(500, 1, 0);
-  rotate_to_angle(270, CCW);
 
-  // B -> TZ1 {
-  forward(500, 3, 270);
-  rotate_to_angle(180, CCW);
-  l2line();
-  backward_time(200, 180, 4000 );
-  backward(10);
-  forward(300, 1, 180);
-
-  // TZ1 -> B
-  rotate_to_angle(90, CCW);
-  forward(500, 3, 90);
-
-  // B -> C
-  rotate_to_angle(0, CCW);
-  forward(500, 1, 0);
-
-  // C -> TZ2
-  rotate_to_angle(270, CCW);
-  forward(500, 3, 270);
-  rotate_to_angle(180, CCW);
-  l2line();
-  backward_time(200, 180, 4000);
-  forward(300, 1, 180);
-
-  // TZ2 -> LOAD
-  rotate_to_angle(90, CCW);
-  forward(500, 2,  90);
-  rotate_to_angle(270, CCW);
-  l2line();
-
-  // LOAD -> TZ3
-  forward(500, 5, 270);
-  rotate_to_angle(180, CCW);
-  backward_time(200, 180, 4000);
-  forward(500, 1, 180);
-
-  // TZ3 -> LOAD
-  rotate_to_angle(90, CCW);
-  forward(500, 5, 90);
-
+//while(1){
+//  Serial.println(encoder());
+//  }
+//  forward_enc(500, 3300, 0);
+//  STOP
+//  delay(500);
+//  forward(500, 5000, 0);
+//  delay(500);
+//
+//
+forward_enc(1000, 14200, 0, 1500);
+STOP
+  write_motors(+27, +300, -300);
+  delay(1000);
+  stop_all();
+  delay(500);
+//  // ARSZ  -> A
+  forward(1000, 14400, 0);
   STOP
+  delay(500);
+  rotate_to_angle(270, CCW);
+
+  // A -> LOAD
+//  backward_enc(1000, -5000, 0);
+//  STOP
+  forward_enc(1000, 3300, 270);
+  delay(1000);
+  // LOAD -> TZ1
+  forward_enc(1000, 5000, 270);
+  delay(500);
+  rotate_to_angle(0, CW);
+  delay(1000);
+  backward_enc(250, -2100, 0, true);
+  forward_enc(1000, 2000, 0);
+  rotate_to_angle(270, CCW);
+  backward_enc(1000, -5000, 270);
+  
+  STOP
+//----------------------------
+  // TZ1 -> LOAD
+  rotate_to_angle(90, CCW);
+  forward_enc(350, 5000, 90);
+
+  delay(1000);
+
+  // LOAD -> A
+  forward(350, 3300, 90);
+
+  // A -> B
+  rotate_to_angle(0, CCW);
+  forward(1000, 6200, 0);
+  
 }
--- automatic_sabretooth/movements.ino
+++ automatic_sabretooth/movements.ino
@@ -1,61 +1,170 @@
-void backward(int speed) {
-  ramp1(0);
-  ramp2(0);
-  write_motors(BRAKE, -speed, -speed);
-}
-
-void forward(int speed, int linecount, double target) {
-  ramp1(2000);
-  ramp2(2000);
-  setpoint = target;
-  input = get_yaw();
-  mkpid(kp, ki, kd);
-  ctr1 = 0;
-  ctr1flag = true;
-  long long tstart = millis();
-  while (true) {
-    Serial.println("ctr1 count: " + String(ctr1));
-    if (ctr1 == linecount) {
-      ramp1(0);
-      ramp2(0);
-      mkpid(kp, ki, kd);
-      speed = 200;
-      ctr1flag = false;
-      ctr1 = 0;
-      ctr2 = 0;
-      ctr2flag = true;
-      while (true) {
-        if (line() >= 190) {
-          Serial.println("stopping");
-          stop_all();
-          ctr2flag = false;
-          ctr2 = 0;
-          return;
-        }  else {
-          Serial.print("[forward PID]: " );
-          Serial.print(output); Serial.print(" ");
-          Serial.print(input); Serial.print(" ");
-          Serial.print(setpoint); Serial.println(" ");
-          input = get_yaw();
-          myPID.Compute();
-          write_motors(BRAKE, speed, speed + output);
-        }
-      }
-
-    } else {
-      Serial.print("[forward PID]: ");
-      Serial.print(output); Serial.println(" ");
-      Serial.print(input); Serial.println(" ");
-      Serial.print(setpoint); Serial.println(" ");
-      input = get_yaw();
-      myPID.Compute();
-      write_motors(BRAKE, speed, speed + output);
-    }
-
-  }
-
-}
-
+//void backward(int speed) {
+//  ramp1(0);
+//  ramp2(0);
+//  write_motors(BRAKE, -speed, -speed);
+//}
+//
+//void back_sun(int speed, int linecount, double target, int ignore=0) {
+//  ramp1(0);
+//  ramp2(0);
+//  setpoint = target;
+//  input = get_yaw();
+//  mkpid(kp, ki, kd);
+//
+//  int cur_lc = 0;
+//  long start_time = millis();
+//
+//  while (true) {
+//    if (line() >= LINE_THRES && (millis() - start_time >= ignore)) {
+//      cur_lc++;
+//      if (cur_lc == linecount) {
+//        stop_all();
+//        return;
+//      }
+//    } else {
+//      input = get_yaw();
+//      myPID.Compute();
+//      write_motors(BRAKE, -(speed + output), -speed);
+//    }
+//  }
+//}
+//
+//void backward(int speed, int linecount, double target) {
+//
+//  ramp1(0);
+//  ramp2(0);
+//
+//  int initial = 0;
+//
+//  setpoint = target;
+//  input = get_yaw();
+//  mkpid(kp, ki, kd);
+//
+//  int cur_lc = 0;
+//
+//  while (true) {
+//    if (line() >= LINE_THRES) {
+//      cur_lc++;
+//      if (cur_lc >= linecount) {
+//        stop_all();
+//        break;
+//      }
+//    } else {
+//
+//      int final_val = ALPHA * initial + ( 1 - ALPHA ) * speed;
+//      initial = final_val;
+//
+//      input = get_yaw();
+//      myPID.Compute();
+//      Serial.print("[backward PID]: " );
+//      Serial.print(output); Serial.print(" ");
+//      Serial.print(input); Serial.print(" ");
+//      Serial.print(setpoint); Serial.println(" ");
+////      write_motors(BRAKE, -(speed + output), -speed);
+//      write_motors(BRAKE, -(final_val + output), -speed);
+//    }
+//  }
+//}
+//
+//void fwd_enc(int speed, long count, int target) {
+//  ramp1(0);
+//  ramp2(0);
+//  reset_encoder();
+//  setpoint = target;
+//  mkpid(kp, ki, kd);
+//  while (1) {
+//    if (encoder() >= count) {
+//      stop_all();
+//      return;
+//    } else {
+//      input = get_yaw();
+//      myPID.Compute();
+//      write_motors(BRAKE, +speed, +speed + output);
+//    }
+//  }
+//}
+//
+//void fwd_sun(int speed, int linecount, double target, int ignore=0) {
+//  ramp1(2000);
+//  ramp2(2000);
+//  setpoint = target;
+//  input = get_yaw();
+//  mkpid(kp, ki, kd);
+//
+//  int cur_lc = 0;
+//  long start_time = millis();
+//
+//  while (true) {
+//    if (line() >= LINE_THRES && (millis() - start_time >= ignore)) {
+//      cur_lc++;
+//      if (cur_lc == linecount) {
+//        stop_all();
+//        return;
+//      }
+//    } else {
+//      input = get_yaw();
+//      myPID.Compute();
+//      write_motors(BRAKE, speed, speed + output);
+//    }
+//  }
+//}
+//
+//
+//
+//void forward(int speed, int linecount, double target, int ign = 0) {
+//  ramp1(2000);
+//  ramp2(2000);
+//  setpoint = target;
+//  input = get_yaw();
+//  mkpid(kp, ki, kd);
+//  ctr1 = 0;
+//  ctr1flag = false;
+//  long long tstart = millis();
+//  while (true) {
+//    Serial.println("ctr1 count: " + String(ctr1));
+//    if ((millis() - tstart) >= ign) ctr1flag = true;
+//    if (ctr1 == linecount) {
+//      ramp1(0);
+//      ramp2(0);
+//      mkpid(kp, ki, kd);
+//      speed = 200;
+//      ctr1flag = false;
+//      ctr1 = 0;
+//      ctr2 = 0;
+//      ctr2flag = true;
+//      while (true) {
+//        if (line() >= LINE_THRES) {
+//          Serial.println("stopping");
+//          stop_all();
+//          ctr2flag = false;
+//          ctr2 = 0;
+//          return;
+//        }  else {
+//          Serial.print("[forward PID]: " );
+//          Serial.print(output); Serial.print(" ");
+//          Serial.print(input); Serial.print(" ");
+//          Serial.print(setpoint); Serial.println(" ");
+//          input = get_yaw();
+//          myPID.Compute();
+//          write_motors(BRAKE, speed, speed + output);
+//        }
+//      }
+//
+//    } else {
+//      Serial.print("[forward PID]: ");
+//      Serial.print(output); Serial.println(" ");
+//      Serial.print(input); Serial.println(" ");
+//      Serial.print(setpoint); Serial.println(" ");
+//      input = get_yaw();
+//      myPID.Compute();
+//      write_motors(BRAKE, speed, speed + output);
+//    }
+//
+//  }
+//
+//}
+//
+//
 
 void rotate_to_angle(double target, byte dir) {
   ramp1(0);
@@ -81,61 +190,156 @@ void rotate_to_angle(double target, byte dir) {
 
   }
 }
+//
+//void backward_time(int speed, int target, int time) {
+//  setpoint = target;
+//  input = get_yaw();
+//  mkpid(kp, ki, kd);
+//  long long cur_time = millis();
+//  while (1) {
+//    if (millis() - cur_time >= time) {
+//      stop_all();
+//      return;
+//    }
+//    input = get_yaw();
+//    myPID.Compute();
+//    write_motors(BRAKE, -speed, -speed);
+//  }
+//}
+//
+//void f2line(double tg) {
+//  ramp1(0);
+//  ramp2(0);
+//  mkpid(kp, ki, kd);
+//  setpoint = tg;
+//  while (true) {
+//    double val = line();
+//    if (val >= 200) break;
+//    input = get_yaw();
+//    myPID.Compute();
+//    write_motors(BRAKE, 100 + output, 100);
+//  }
+//  stop_all();
+//}
+//
+//void l2line() {
+//  Serial.println("[l2line] l2line running");
+//  ramp1(0);
+//  ramp2(0);
+//  write_motors(+27, +300, -300);
+//  while (digitalRead(2) == HIGH);
+//  stop_all();
+//  Serial.println("[l2line] l2line done");
+//}
+//
+//
+//void forward_time(int speed, int dir, int d) {
+//  setpoint = dir;
+//  input = get_yaw();
+//  mkpid(kp, ki, kd);
+//  long long cur_time = millis();
+//  while (1) {
+//    if (millis() - cur_time >= d) {
+//      stop_all();
+//      return;
+//    }
+//    input = get_yaw();
+//    myPID.Compute();
+//    write_motors(BRAKE, speed + output, speed);
+//  }
+//}5\
 
-void backward_time(int speed, int target, int time) {
+void forward_enc(int speed, int enc, int target, int offset=1500) {
   setpoint = target;
-  input = get_yaw();
   mkpid(kp, ki, kd);
-  long long cur_time = millis();
-  while (1) {
-    if (millis() - cur_time >= time) {
-      stop_all();
-      return;
-    }
-    input = get_yaw();
-    myPID.Compute();
-    write_motors(BRAKE, -speed, -speed);
-  }
-}
+  reset_encoder();
 
-void f2line(double tg) {
-  ramp1(0);
-  ramp2(0);
-  mkpid(kp, ki, kd);
-  setpoint = tg;
-  while (true) {
-    double val = line();
-    if (val >= 200) break;
+  while ((enc - encoder()) > offset) {
     input = get_yaw();
     myPID.Compute();
-    write_motors(BRAKE, 100 + output, 100);
+    Serial.print("[pid]: ");
+    Serial.print(output); Serial.print(" ");
+    Serial.print(input); Serial.print(" ");
+    Serial.print(setpoint); Serial.println(" ");
+    write_motors(BRAKE, speed , speed + output) ;
+    Serial.println(encoder());
   }
+
+  speed = 130;
+  mkpid(kp, ki, kd);
+
+  while ((enc-encoder()) > 0) {
+    input = get_yaw();
+    myPID.Compute();
+    write_motors(BRAKE, speed, speed + output);
+    Serial.println(encoder());
+  }
+  Serial.println("Stop.");
   stop_all();
 }
 
-void l2line() {
-  Serial.println("[l2line] l2line running");
-  ramp1(0);
-  ramp2(0);
-  write_motors(+27, +300, -300);
-  while (digitalRead(2) == HIGH);
-  stop_all();
-  Serial.println("[l2line] l2line done");
-}
-
-
-void forward_time(int speed, int dir, int d) {
-  setpoint = dir;
-  input = get_yaw();
+void forward(int speed, int enc, int target) {
+  setpoint = target;
   mkpid(kp, ki, kd);
-  long long cur_time = millis();
-  while (1) {
-    if (millis() - cur_time >= d) {
-      stop_all();
-      return;
-    }
+  reset_encoder();
+
+  while (enc - encoder() > 800) {
     input = get_yaw();
     myPID.Compute();
-    write_motors(BRAKE, speed + output, speed);
+    Serial.print("[pid]: ");
+    Serial.print(output); Serial.print(" ");
+    Serial.print(input); Serial.print(" ");
+    Serial.print(setpoint); Serial.println(" ");
+    write_motors(BRAKE, speed , speed + output) ;
   }
+
+  speed = 200;
+  mkpid(kp, ki, kd);
+
+  while (line() < LINE_THRES) {
+    input = get_yaw();
+    myPID.Compute();
+    Serial.print("[pid]: ");
+    Serial.print(output); Serial.print(" ");
+    Serial.print(input); Serial.print(" ");
+    Serial.print(setpoint); Serial.println(" ");
+    write_motors(BRAKE, speed , speed + output);
+  }
+
+  stop_all();
+
+}
+void backward_enc(int speed, int enc, int target, bool fence=false) {
+  setpoint = target;
+  mkpid(kp, ki, kd);
+  reset_encoder();
+  long long start=millis();
+  while (abs(enc - encoder()) > 1500) {
+    input = get_yaw();
+    myPID.Compute();
+    Serial.print("[pid]: ");
+    Serial.print(output); Serial.print(" ");
+    Serial.print(input); Serial.print(" ");
+    Serial.print(setpoint); Serial.println(" ");
+    write_motors(BRAKE, -speed , -speed+output) ;
+    Serial.println(encoder());
+    if(fence && (millis()-start)>1500)
+    break;
+  }
+  if(fence)
+  {
+    stop_all();
+    return;
+    }
+  speed = 120;
+  mkpid(kp, ki, kd);
+
+  while ((enc-encoder()) < 0) {
+    input = get_yaw();
+    myPID.Compute();
+    write_motors(BRAKE, -speed, -speed + output);
+    Serial.println(encoder());
+  }
+  Serial.println("Stop.");
+  stop_all();
 }
